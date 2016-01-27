f541d2b4cc3e3a606a92220d2cdf739d8cd591ea
naldojesse <naldojesse@gmail.com>
2016-01-27 14:48:36 -0800

    updated friend dashboard

--- client/static/javascript/controllers/frienddashboardController.js
+++ client/static/javascript/controllers/frienddashboardController.js
@@ -1,3 +1,43 @@
-dak_app.controller('frienddashboardController', function() {
+dak_app.controller('frienddashboardController', function($route, friendFactory, userFactory, $cookies) {
 	
+	this.userId = $cookies.get('userId')
+	this.userName = $cookies.get('userName');
+
+	this.users = [];
+
+	this.user;
+
+	var _this = this;
+	userFactory.index(function(data) {
+		console.log(data);
+		_this.users = data;
+	})
+
+	userFactory.show(this.userId, function(data) {
+		console.log(data);
+		_this.user = data;
+	})
+
+	this.addFriend = function(friend) {
+
+		var newFriend = {};
+		newFriend.userID = this.userId;
+		newFriend.friendID = friend._id;
+
+		friendFactory.create(this.userId, newFriend)
+		$route.reload();
+
+	} 
+
+	this.removeFriend = function(friend) {
+
+		var removeFriend = {};
+		removeFriend.userID = this.userId;
+		removeFriend.friendID = friend._id;
+
+		friendFactory.destroy(this.userId, removeFriend)
+		$route.reload();
+	}
+
+
 })
